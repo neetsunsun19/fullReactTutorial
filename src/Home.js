0ca62e222e095e47e15f8fe1b77d9be6d799993b
neetsunsun19 <neetsunsun19@gmail.com>
2021-07-13 23:01:06 +0800

    lesson 19

--- src/Home.js
+++ src/Home.js
@@ -3,38 +3,48 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     // const [blogs, setBlogs] = useState([
     //     {title:'My New Website',body:'Hey there this is new',author:'mario',id:1},
     //     {title:'My New Website2',body:'Hey there this is new2',author:'neet',id:2},
     //     {title:'My New Website3',body:'Hey there this is new3',author:'mario',id:3}
     // ]);
     
-    const [name, setName] = useState('mario');
+    // const [name, setName] = useState('mario');
 
-    const handleDelete = (id) => {
-        //filter, if condition = false, it will remove from the array
-        //if id does not match, it gets to stay
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs)
-    }
+    // const handleDelete = (id) => {
+    //     //filter, if condition = false, it will remove from the array
+    //     //if id does not match, it gets to stay
+    //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+    //     setBlogs(newBlogs)
+    // }
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok){
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then(data => {
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err =>{
+                setIsPending(false);
+                setError(err.message);
             })
     },[]); //empty [] is when run the effect once
 
     return (
         <div className="home">
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"></BlogList>}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!"></BlogList> */}
-            <button onClick={() => setName('luigi')}>Change Name</button>
-            <p>{name}</p>
         </div> 
     );
 }
